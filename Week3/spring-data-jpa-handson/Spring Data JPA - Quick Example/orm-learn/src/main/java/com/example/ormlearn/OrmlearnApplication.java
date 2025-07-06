3e7c406f33749f0d90c84a81a9c608fba3dbc909
Tharanya009 <tharanya0904@gmail.com>
2025-07-06 23:50:22 +0530

    Added Week 3 assignments - Spring Data JPA hands-on

---
+++ Week3/spring-data-jpa-handson/Spring Data JPA - Quick Example/orm-learn/src/main/java/com/example/ormlearn/OrmlearnApplication.java
@@ -0,0 +1,12 @@
+package com.example.ormlearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrmlearnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrmlearnApplication.class, args);
+    }
+}
---
+++ Week3/spring-data-jpa-handson/Spring Data JPA - Quick Example/orm-learn/src/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO employee (id, name) VALUES (1, 'Alice');
+INSERT INTO employee (id, name) VALUES (2, 'Bob');
+INSERT INTO employee (id, name) VALUES (3, 'Charlie');
+INSERT INTO employee (id, name) VALUES (4, 'David');
